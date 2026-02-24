2dba88adbfd7d7f7d13a68892416bb1ee2cd7b78
Ubuntu <azureuser@dotnet-vm.wwudqah4n2he1kvx0jmmoz13zh.rx.internal.cloudapp.net>
2026-02-24 18:43:14 +0000

    Added real unit tests for ProductController

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,10 +1,41 @@
-namespace MyApp.Tests;
+using Xunit;
+using MyApp.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
-public class UnitTest1
+namespace MyApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class ProductControllerTests
     {
+        [Fact]
+        public void Get_ReturnsProductList()
+        {
+            var controller = new ProductController();
 
+            var result = controller.Get();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var products = Assert.IsAssignableFrom<IEnumerable<object>>(okResult.Value);
+
+            Assert.NotNull(products);
+        }
+
+        [Fact]
+        public void Add_ReturnsCreatedProduct()
+        {
+            var controller = new ProductController();
+
+            var product = new MyApp.Api.Controllers.Product
+            {
+                Id = 1,
+                Name = "Test Product",
+                Price = 100
+            };
+
+            var result = controller.Add(product);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+        }
     }
 }
--- MyApp.Tests/obj/Debug/net8.0/MyApp.Tests.AssemblyInfo.cs
+++ MyApp.Tests/obj/Debug/net8.0/MyApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e119e4cbece7e070495eefda95f2e806292aa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
